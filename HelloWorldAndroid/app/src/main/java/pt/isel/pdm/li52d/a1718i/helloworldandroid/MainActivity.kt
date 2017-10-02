a5930eeb81b9f95cf7191468d1510ca0e3e6a0df
Luís Falcão <lfalcao@cc.isel.pt>
2017-10-02 15:34:24 +0100

    Aula 2017-10-02-Counter increment and decrement activity using android extensions.

--- HelloWorldAndroid/app/src/main/java/pt/isel/pdm/li52d/a1718i/helloworldandroid/MainActivity.kt
+++ HelloWorldAndroid/app/src/main/java/pt/isel/pdm/li52d/a1718i/helloworldandroid/MainActivity.kt
@@ -4,37 +4,33 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val TAG: String = "MainActivity";
 
 class MainActivity : Activity() {
-    private val COUNTER_KEY = "counter"
+    private val COUNTER_KEY = "currentValue"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val decrement: Button = findViewById(R.id.decrement);
-        val increment: Button = findViewById(R.id.increment);
-
+        counter.text = "";
         increment.setOnClickListener(getOnClickListener("increment", 1));
-        decrement.setOnClickListener(getOnClickListener("derement", -1));
+        decrement.setOnClickListener(getOnClickListener("decrement", -1));
     }
 
-    private var counter: Int
-        get() = findViewById<TextView>(R.id.counter).text.toString().toInt()
-        set(value) {
-            findViewById<TextView>(R.id.counter).text = value.toString()
-        }
+    private var currentValue: Int
+        get() = counter.text.toString().toInt()
+        set(value) { counter.text = value.toString() }
 
 
 private fun getOnClickListener(message: String, delta: Int): View.OnClickListener {
     return View.OnClickListener {
         Log.i(TAG, "$message click called")
-        counter += delta*2;
+        currentValue += delta*2;
     }
 
 }
