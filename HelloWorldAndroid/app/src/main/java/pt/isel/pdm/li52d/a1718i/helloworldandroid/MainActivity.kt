2aea14794a72c4674f3a325636ea3af1bfb34a5b
Luís Falcão <lfalcao@cc.isel.pt>
2017-10-04 12:53:53 +0100

    Aula 2017-10-04-Resolução do Laboratório 3

--- HelloWorldAndroid/app/src/main/java/pt/isel/pdm/li52d/a1718i/helloworldandroid/MainActivity.kt
+++ HelloWorldAndroid/app/src/main/java/pt/isel/pdm/li52d/a1718i/helloworldandroid/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val TAG: String = "MainActivity";
@@ -19,19 +18,39 @@ class MainActivity : Activity() {
 
         increment.setOnClickListener(getOnClickListener("increment", 1));
         decrement.setOnClickListener(getOnClickListener("decrement", -1));
+        
     }
 
     private var currentValue: Int
         get() = counter.text.toString().toInt()
-        set(value) { counter.text = value.toString() }
+        set(value) {
+            counter.text = value.toString()
+        }
+
+    private var maxValue: Int = 0
+        get() = max.text.toString().toInt() +1
 
 
-private fun getOnClickListener(message: String, delta: Int): View.OnClickListener {
-    return View.OnClickListener {
-        Log.i(TAG, "$message click called")
-        currentValue += delta;
+    private fun getOnClickListener(message: String, delta: Int): View.OnClickListener {
+        return View.OnClickListener {
+            Log.i(TAG, "$message click called")
+
+            currentValue = (currentValue + maxValue + delta) % maxValue
+        }
+
     }
 
-}
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState called");
+        outState?.putInt(COUNTER_KEY, currentValue);
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState called");
+        currentValue = savedInstanceState?.getInt(COUNTER_KEY) ?: 0
+
+    }
 
 }
